7084970e975af153098476b24e20657e3cb7981b
Anastasia Moroz <anfrostdev@gmail.com>
2024-09-29 20:40:39 +0300

    Added module function to validate matrix multiplication capability

--- src/matrix_math_module.cpp
+++ src/matrix_math_module.cpp
@@ -1,6 +1,39 @@
 #include <Python.h>
 
+static PyObject* validate_possibility_multiply_matrix(PyObject* self, PyObject* args) {
+    PyObject* pyCfgMatrixOne = nullptr;
+    PyObject* pyCfgMatrixTwo = nullptr;
+
+    if (!PyArg_ParseTuple(args, "OO", &pyCfgMatrixOne, &pyCfgMatrixTwo)) {
+        PyErr_SetString(PyExc_Exception, "Not valid arguments in function possibility_multipl (c++)");
+
+        return nullptr;
+    }
+
+    if (PyList_Size(pyCfgMatrixOne) != 3) {
+        PyErr_SetString(PyExc_Exception, "Not three arguments in config matrix one (c++)");
+
+        return nullptr;
+    }
+
+    if (PyList_Size(pyCfgMatrixTwo) != 3) {
+        PyErr_SetString(PyExc_Exception, "Not three arguments in config matrix two (c++)");
+
+        return nullptr;
+    }
+
+    int columnMatrixOne = PyLong_AsLong(PyList_GetItem(pyCfgMatrixOne, 1));
+    int rowMatrixTwo = PyLong_AsLong(PyList_GetItem(pyCfgMatrixTwo, 0));
+
+    if (columnMatrixOne == rowMatrixTwo) {
+        Py_RETURN_TRUE;
+    }
+
+    Py_RETURN_FALSE;
+}
+
 static PyMethodDef matrix_math_methods[] = {
+    { "validate_possibility_multiply_matrix", (PyCFunction)validate_possibility_multiply_matrix, METH_O, nullptr },
     { nullptr, nullptr, 0, nullptr }
 };
 
